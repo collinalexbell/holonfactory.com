a4214058fc51a754e9f8c7ebe6168cb663660c80
kuberlog <collinalexbell@gmail.com>
2022-04-02 02:15:51 +0000

    sfml init

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 #include "holon_factory.h"
+#include <SFML/Graphics.hpp>
 
-int main() {
+int main()
+{
+  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+  sf::CircleShape shape(100.f);
+  shape.setFillColor(sf::Color::Green);
 
   Factory f("Tree");
   cout << "Welcome to the factory town of " << f.getName() << endl;
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
 
+    window.clear();
+    window.draw(shape);
+    window.display();
+  }
+
+  return 0;
 }
+
